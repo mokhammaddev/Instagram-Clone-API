a1b8ffd0b2da5667525f0432d7c79e9b087ebb48
muhammad <muhammadtoychiboyev1@gmail.com>
2024-02-28 22:46:01 +0500

    Instagram-Clone

--- conf/gunicorn_config.py
+++
@@ -1,4 +0,0 @@
-command = '/home/ubuntu/Desktop/Projects/Instagram-Clone-API/venv/bin/gunicorn'
-pythonpath = '/home/ubuntu/Desktop/Projects/Instagram-Clone-API/config'
-bind = '16.171.148.146:8000'
-workers = 3
